d814a38321cd00aabefa52c1c7670d12f4e2fb30
keunsulee <i.knsulee@gmail.com>
2016-09-09 16:33:39 +0900

    ....

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.tourguide;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter simpleFragmentPagerAdapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(simpleFragmentPagerAdapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
---
+++ app/src/main/java/com/example/android/tourguide/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,24 @@
+package com.example.android.tourguide;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by donbosco on 2016-09-09.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
